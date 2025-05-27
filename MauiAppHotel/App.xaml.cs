edf31b8d68fe95fe06d2dc8beb9606677f8e0f1e
plabsjc <pedro.lucas.sjc@gmail.com>
2025-05-27 10:29:35 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = new Window(new AppShell());
+        #if WINDOWS
+            window.Width = 400;
+            window.Height = 800;
+        #endif
+
+            return window;
+
+        }
+    }
+}
---
+++ MauiAppHotel/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiAppHotel;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
