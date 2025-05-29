b3090f983f9b525ffc9a511c662db97b2e7270f5
plabsjc <pedro.lucas.sjc@gmail.com>
2025-05-29 04:26:57 -0300

    Correção na regra de negócio, pelo menos um adulto deve ser selecionado.

---
+++ MauiAppHotel/Models/Hospedagem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppHotel.Models
+{
+    public class Hospedagem
+    {
+        public Quarto QuartoSelecionado { get; set; }
+        public int QuantidadeAdultos { get; set; }
+
+        public int QuantidadeCriancas { get; set; }
+
+        public DateTime DataCheckIn { get; set; }
+
+        public DateTime DataCheckOut { get; set; }
+
+        public int Estadia { get => DataCheckOut.Subtract(DataCheckIn).Days; }
+
+        public double ValorTotal { 
+            get => (QuantidadeAdultos * QuartoSelecionado.ValorDiariaAdulto 
+                + QuantidadeCriancas * QuartoSelecionado.ValorDiariaCrianca) * Estadia;
+        }
+
+
+
+    }
+}
--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -1,3 +1,4 @@
+﻿using MauiAppHotel.Models;
 using System.Threading.Tasks;
 
 namespace MauiAppHotel.Views;
@@ -9,28 +10,63 @@ public partial class ContratacaoHospedagem : ContentPage
     {
         InitializeComponent();
 
+        var hoje = DateTime.Today;
+
         PropriedadesApp = (App)Application.Current;
 
         pickerQuarto.ItemsSource = PropriedadesApp.listaQuartos;
 
-        datePickerCheckIn.MinimumDate = DateTime.Now;
-        datePickerCheckIn.MaximumDate = DateTime.Now.AddDays(30);
-        datePickerCheckOut.MinimumDate = datePickerCheckIn.Date.AddDays(1);
-        datePickerCheckOut.MaximumDate = datePickerCheckIn.Date.AddDays(7);
-        
+        datePickerCheckIn.MinimumDate = hoje;
+        datePickerCheckIn.MaximumDate = hoje.AddDays(30);
+        datePickerCheckIn.Date = hoje;
+        //datePickerCheckOut.MinimumDate = datePickerCheckIn.Date.AddDays(1);
+        //datePickerCheckOut.MaximumDate = datePickerCheckIn.Date.AddDays(7);
+        var dataSelecionadaCheckIn = datePickerCheckIn.Date;
+        datePickerCheckOut.MinimumDate = dataSelecionadaCheckIn.AddDays(1);
+        datePickerCheckOut.MaximumDate = dataSelecionadaCheckIn.AddDays(7);
+        datePickerCheckOut.Date = dataSelecionadaCheckIn.AddDays(1);
+
 
 
     }
      
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
         try
         {
-            Navigation.PushAsync(new HospedagemContratada());
+            if((Quarto)pickerQuarto.SelectedItem == null)
+            {
+                await DisplayAlert("Ops!!!", "Selecione o tipo de Suíte!", "Ok");
+            }
+            else if (stepperAdultos.Value == 0)
+            {
+                await DisplayAlert("Ops!!!", "Selecione ao menos uma pessoa adulta!", "OK");
+            }
+            else if (datePickerCheckOut.Date <= datePickerCheckIn.Date)
+            {
+                await DisplayAlert("Ops!!!", "Data CheckOut menor ou igual a data do CheckIn", "OK");
+            }
+            else
+            {
+                Hospedagem h = new Hospedagem
+                {
+                    QuartoSelecionado = (Quarto)pickerQuarto.SelectedItem,
+                    QuantidadeAdultos = Convert.ToInt32(stepperAdultos.Value),
+                    QuantidadeCriancas = Convert.ToInt32(stepperCriancas.Value),
+                    DataCheckIn = datePickerCheckIn.Date,
+                    DataCheckOut = datePickerCheckOut.Date
+                };
+
+                await Navigation.PushAsync(new HospedagemContratada()
+                {
+                    BindingContext = h
+                });
+            }
+                
         }
         catch (Exception ex)
         {
-            DisplayAlert("Ops", ex.Message, "OK");
+            await DisplayAlert("Ops", ex.Message, "OK");
         }
     }
 
@@ -38,7 +74,16 @@ public partial class ContratacaoHospedagem : ContentPage
     {
         DatePicker elemento = sender as DatePicker;
         DateTime dataSelecionadaCheckIn = elemento.Date;
-        datePickerCheckOut.MinimumDate = dataSelecionadaCheckIn.AddDays(1);
-        datePickerCheckOut.MaximumDate = dataSelecionadaCheckIn.AddDays(30);
+        var novaMinima = dataSelecionadaCheckIn.AddDays(1);
+        var novaMaxima = dataSelecionadaCheckIn.AddDays(7);
+
+        datePickerCheckOut.MinimumDate = novaMinima;
+        datePickerCheckOut.MaximumDate = novaMaxima;
+
+        // ⚠️ Corrige a data se estiver fora do intervalo
+        if (datePickerCheckOut.Date < novaMinima || datePickerCheckOut.Date > novaMaxima)
+        {
+            datePickerCheckOut.Date = novaMinima;
+        }
     }
 }
