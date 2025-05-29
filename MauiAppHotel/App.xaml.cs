f457386b39f758a074657026f0052f013e743bbe
plabsjc <pedro.lucas.sjc@gmail.com>
2025-05-29 00:44:19 -0300

    Acrescentando regras de negócio sobre o período de estadia e corrigindo estadia negativa.

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,26 +1,51 @@
-﻿namespace MauiAppHotel
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
 {
     public partial class App : Application
     {
+        public List<Quarto> listaQuartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiariaAdulto = 110.00,
+                ValorDiariaCrianca = 55.00
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 90.00,
+                ValorDiariaCrianca = 45.00
+            },
+
+            new Quarto()
+            {
+                Descricao = "Suíte Simples",
+                ValorDiariaAdulto = 80.00,
+                ValorDiariaCrianca = 40.00
+            }
+        };
         public App()
         {
             InitializeComponent();
             MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
-          
+
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
             var window = base.CreateWindow(activationState);
-            #if WINDOWS
+#if WINDOWS
             window.Width = 400;
             window.Height = 800;
-            #endif
+#endif
 
             return window;
 
         }
 
-        
+
     }
 }
--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -4,11 +4,24 @@ namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
+    App PropriedadesApp;
     public ContratacaoHospedagem()
     {
         InitializeComponent();
-    }
 
+        PropriedadesApp = (App)Application.Current;
+
+        pickerQuarto.ItemsSource = PropriedadesApp.listaQuartos;
+
+        datePickerCheckIn.MinimumDate = DateTime.Now;
+        datePickerCheckIn.MaximumDate = DateTime.Now.AddDays(30);
+        datePickerCheckOut.MinimumDate = datePickerCheckIn.Date.AddDays(1);
+        datePickerCheckOut.MaximumDate = datePickerCheckIn.Date.AddDays(7);
+        
+
+
+    }
+     
     private void Button_Clicked(object sender, EventArgs e)
     {
         try
@@ -19,5 +32,13 @@ public partial class ContratacaoHospedagem : ContentPage
         {
             DisplayAlert("Ops", ex.Message, "OK");
         }
+    }
+
+    private void datePickerCheckIn_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        DatePicker elemento = sender as DatePicker;
+        DateTime dataSelecionadaCheckIn = elemento.Date;
+        datePickerCheckOut.MinimumDate = dataSelecionadaCheckIn.AddDays(1);
+        datePickerCheckOut.MaximumDate = dataSelecionadaCheckIn.AddDays(30);
     }
 }
